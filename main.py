c07bd9c4576f75d8c70a78fb56ea2e6676be1458
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-07 17:50:56 +0300

    Fixed main logic

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
-import pygame,sys
+import pygame, sys
 from pygame.locals import *
 from game.menu import menu
-from game.howto import howto
+from game.game import game
 
 pygame.init()
+
 def main():
-    display=pygame.display.set_mode((800,800))
+    display = pygame.display.set_mode((800, 800))
     pygame.display.set_caption('Flip 7')
-    clock=pygame.time.Clock()
-    status="menu"
+    clock = pygame.time.Clock()
+    status = "menu"
+    selectedPlayers = None
+
     while True:
-        if status =="menu":
-            result=menu(display,clock)
-            if result=="howto":
-                status="howto"
-            continue
-        elif status=="howto":
-            result=howto(display,clock)
-            if result=="menu":
-                status="menu"
+        if status == "menu":
+            result = menu(display, clock)
+            selectedPlayers = result
+            status = "game"
+        elif status == "game":
+            game(display, clock, selectedPlayers)
+            status = "menu" 
         pygame.display.update()
         clock.tick(60)
-if __name__=='__main__':
+
+if __name__ == '__main__':
     main()
-
-
-
