d75fc7ea485c318b36130508e3bec2bcb9ac1f00
Nazrawit-T-B <nazrawitbezabih@gmail.com>
2026-09-07 17:51:51 +0300

    Initalized card class and deck for game

--- game/game.py
+++ game/game.py
@@ -1,8 +1,24 @@
 import pygame,sys
 from pygame.locals import *
-from UI.button import Button
-def game(display,clock):
-    # Placeholder for the game logic
+#from UI.button import Button
+from card import Card,CardType
+import random
+
+def game(display,clock,selectedPlayers):
+    
+    
+    NumCards=[]
+    for value in range(13):
+        for _ in range(value+1):
+            NumCards.append(Card(CardType.NUMBER,value))
+    
+    print(selectedPlayers)
+
+    ActionCards=[Card(CardType.FREEZE),Card(CardType.FREEZE),Card(CardType.FREEZE),Card(CardType.FLIP_THREE),Card(CardType.FLIP_THREE),Card(CardType.FLIP_THREE),Card(CardType.SECOND_CHANCE),Card(CardType.SECOND_CHANCE),Card(CardType.SECOND_CHANCE)]
+
+    DECK=NumCards+ActionCards
+    
+    random.shuffle(DECK)
     while True:
         for event in pygame.event.get():
             if event.type == QUIT:
